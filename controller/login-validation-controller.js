f6355f480aa400332cb2d34f8be29c7ec784b5c1
mqsoares <mqseraos@gmail.com>
2023-03-24 00:37:27 -0300

    Login autentication

--- controller/login-validation-controller.js
+++ controller/login-validation-controller.js
@@ -2,5 +2,19 @@ const btnEnter = document.querySelector('[data-enter]')
 
 btnEnter.addEventListener('click', (e) => {
     e.preventDefault()
-    window.location.href = './listProducts.html'
+
+    const user = 'alurageek@alura.com'
+    const pass = '1234'
+
+    const inputNameLog = document.querySelector('[data-lgname]').value
+    const inputPasswordLog = document.querySelector('[data-lgpassword]').value
+    
+    
+    // console.log(inputNameLog, inputPasswordLog)
+    if (inputNameLog === user && inputPasswordLog === pass){
+        window.location.href = './listProducts.html'
+    } else {
+        alert('OPS! "Usuário" ou "Senha" inválidos')               
+    } 
+    
 })
