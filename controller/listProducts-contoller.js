2068b077423c99b1057ebbff1ea5356517ce0d9d
mqsoares <mqseraos@gmail.com>
2023-03-23 23:17:53 -0300

    Create button delete and edit card

--- controller/listProducts-contoller.js
+++ controller/listProducts-contoller.js
@@ -11,10 +11,16 @@ const newProduct = (id, imgURL, alt, name, price, description) => {
     const card = document.createElement('div');        
     const template = `
         <div class="products-body">
-            <img src="${imgURL}" alt="${alt}">
-            <p class="product-name">${name}</p>
-            <p class="product-price">R$ ${price}</p>
-            <p class="product-detail">Ver Produto</p>
+            <img class="products-body-img" src="${imgURL}" alt="${alt}">
+            <div class="product-resume">
+                <p class="product-name">${name}</p>
+                <p class="product-price">R$ ${price}</p>
+                <p class="product-detail">Ver Produto</p>
+            </div>
+            <span class="product-btn-control">
+                <a href="#"><img src="../assets/img/del.svg" alt="botão excluir" class="btn-simple btn-del" ></a>
+                <a href="#"><img src="../assets/img/edit.svg" alt="botão editar" class="btn-simple btn-edit "></a>
+            </span>
         </div>
     `
     card.innerHTML = template
